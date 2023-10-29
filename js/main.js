f19b153d8b4123c7151a0ead37ee7f4a9dc70535
seohyunCho <shanny06@naver.com>
2023-10-30 00:45:38 +0900

    main페이지 구현

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function goTocurrectPage(index){
+    if(index === 0){
+        window.location.href = '/share.html';
+    }else if(index === 1){
+        window.location.href = '/together.html';
+    }else if(index === 2){
+        window.location.href = '/learn.html';
+    }
+}
