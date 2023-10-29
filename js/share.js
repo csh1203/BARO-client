bffdd45a51020cb92467cb6bdb0cc01fe53c4cad
seohyunCho <shanny06@naver.com>
2023-10-30 00:45:59 +0900

    나누기 화면 생성 및 구현

---
+++ js/share.js
@@ -0,0 +1,7 @@
+let stuId = document.getElementsByClassName('stu-id')[0];
+let id = "2314";
+let name = "조서현"
+stuId.innerHTML = `${id}_${name}`;
+
+commentCnt = document.getElementsByClassName('comment-cnt-num')[0];
+commentCnt.innerHTML = 3;
