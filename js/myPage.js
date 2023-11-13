c7d12f2a820f7da854a64918585fc0c0e4306e63
seohyunCho <shanny06@naver.com>
2023-11-13 22:45:49 +0900

    myPage 화면 구성

---
+++ js/myPage.js
@@ -0,0 +1,7 @@
+function backHome(){
+    window.location.href = "/main.html"
+}
+
+document.getElementsByClassName("student-id")[0].innerHTML = "2314_조서현";
+document.getElementsByClassName("major")[0].innerHTML = "뉴미디어웹솔루션과";
+document.getElementsByClassName("level-name")[0].innerHTML = "Lv.1 거북이"
