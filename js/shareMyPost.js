a21ac3663156b16fe58260eceee9da5ff8d5125c
seohyunCho <shanny06@naver.com>
2023-11-15 01:17:19 +0900

    나의 글 수정 및 삭제 버튼 생성

--- js/shareMyPost.js
+++ js/shareMyPost.js
@@ -16,12 +16,45 @@ function backHome(){
     window.location.href = "/main.html";
 }
 
-let editArr = document.getElementsByClassName("edit-content-btn");
-for(let i in editArr){
-    editArr[i].onclick = () => editContent(i);
+let index = -1;
+document.addEventListener('click', (e) => {
+    let editDiv = document.getElementsByClassName('edit-post-div')[0];
+    if(e.target.className != "edit-content"){
+        editDiv.style.visibility = "hidden";
+    }
+    
+});
+let editArr = [...document.getElementsByClassName("edit-content-btn")];
+editArr.forEach((e, i) => {
+    e.onclick = () => showEditDiv(e, i);
+});
+
+function showEditDiv(e, i){
+    let editDiv = document.getElementsByClassName('edit-post-div')[0];
+    if(index != i){
+        let buttonRect = e.getBoundingClientRect();
+        let buttonX = buttonRect.left + window.pageXOffset;
+        let buttonY = buttonRect.top + window.pageYOffset;
+        editDiv.style.visibility = "visible";
+        editDiv.style.top = `${buttonY}px`;
+        editDiv.style.left =`${buttonX - 80}px`;
+        index = i;
+    }else{
+        editDiv.style.visibility = "hidden";
+        index = -1;
+    }
+
+    let editButton = document.getElementsByClassName('edit-post')[0];
+    let deleteButton = document.getElementsByClassName('delete-post')[0];
+    editButton.onclick = () => editMyPost(i);
+    deleteButton.onclick = () => deleteMyPost(i);
 }
 
-function editContent(i){
-    let button = editArr[i];
-    // let buttonRect = button.
+function editMyPost(i){
+    console.log(i);
+    window.location.href = "/shareEditMyPost.html";
+}
+
+function deleteMyPost(i){
+    console.log(i);
 }
