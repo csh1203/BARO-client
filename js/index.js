04e6990aab175bc8fc3191fbd9f194fe06e654d7
이서영 <88700475+seoyoung1276@users.noreply.github.com>
2023-11-15 07:40:01 +0900

    푸시

--- js/index.js
+++ js/index.js
@@ -1,4 +1,13 @@
+const loginButton = document.getElementsByClassName('logIn')[0]
+
 function logIn(){
+
+
+
+    const response = axios.get(`${BASE_URL}/google`);
+    console.log(response.data)
+
+
     window.location.href = '/main.html';
 }
 
