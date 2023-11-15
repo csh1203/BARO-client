fd6374d60dd59562d0734eb8405389a7b06633ed
이서영 <88700475+seoyoung1276@users.noreply.github.com>
2023-11-15 19:41:25 +0900

    코드 정리

--- js/index.js
+++ js/index.js
@@ -1,9 +1,5 @@
 const loginButton = document.getElementsByClassName('logIn')[0];
 
-// Google OAuth 로그인 버튼을 누르면 백엔드로 리다이렉트됩니다.
-loginButton.onclick = () => {
-    // axios.get(`${BASE_URL}/auth/google`)
-
+function handleGoogleLogin() {
+    window.location.href = 'http://localhost:3000/auth/google';
 }
-
-  
