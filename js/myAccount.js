1acf2be176f86a04e9e2e3ac687ee8fc0afe5be4
seohyunCho <shanny06@naver.com>
2023-11-16 15:10:42 +0900

    share 글쓰기, 글 불러오기 시도

--- js/myAccount.js
+++ js/myAccount.js
@@ -25,15 +25,7 @@ axios.get(`${BASE_URL}/auth/userinfo`, { withCredentials: true})
 function logout(){
     axios.get(`${BASE_URL}/auth/logout`)
     .then(response => {
-        // console.log('User info:', response.data);
-        // let grade_info = response.data.name.substring(0, 1);
-        // let class_info = response.data.name.substring(1, 2);
-        // let id_info = response.data.name.substring(2, 4);
-        // stuName.innerText = response.data.name;
-        // stuMajor.innerText = response.data.major;
-        // stuClass.innerText = `${grade_info}학년 ${class_info}반 ${id_info}번`;
-        // stuEmail.innerText = response.data.email;
-        console.log(response)
+        // console.log(response.data)
     })
     .catch(error => {
         console.error('There has been a problem with your axios request:', error);
--- js/shareNewPost.js
+++ js/shareNewPost.js
@@ -3,5 +3,19 @@ function backShare(){
 }
 
 function makePost(){
-    window.location.href = '/share.html'
+    let titleC = document.getElementsByClassName("post-title")[0].value;
+    let contentC = document.getElementsByClassName('post-main')[0].value;
+    
+    const req = {
+        title: titleC,
+        content: contentC
+    }
+    axios.post(`${BASE_URL}/share/post`, req)
+    .then(result => {
+        // console.log(response)
+    })
+    .catch(error => {
+        console.error('There has been a problem with your axios request:', error);
+    });
+    // window.location.href = '/share.html'
 }
