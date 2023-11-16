f81c0fdcd9989c55186c72da286dd39730d5d2f9
이서영 <88700475+seoyoung1276@users.noreply.github.com>
2023-11-16 15:16:58 +0900

    .

--- js/myAccount.js
+++ js/myAccount.js
@@ -23,19 +23,10 @@ axios.get(`${BASE_URL}/auth/userinfo`, { withCredentials: true})
     });
 
 function logout(){
+    try{
     axios.get(`${BASE_URL}/auth/logout`)
-    .then(response => {
-        // console.log('User info:', response.data);
-        // let grade_info = response.data.name.substring(0, 1);
-        // let class_info = response.data.name.substring(1, 2);
-        // let id_info = response.data.name.substring(2, 4);
-        // stuName.innerText = response.data.name;
-        // stuMajor.innerText = response.data.major;
-        // stuClass.innerText = `${grade_info}학년 ${class_info}반 ${id_info}번`;
-        // stuEmail.innerText = response.data.email;
-        console.log(response)
-    })
-    .catch(error => {
+    
+    }catch(error) {
         console.error('There has been a problem with your axios request:', error);
-    });
+    }
 }
